44e53f0eb3d6af7af02e109a5362f378a46bba6f
tomfrew <tom@keel.xyz>
2023-03-19 17:37:35 +0000

    Function permission check

--- functions/setCompletion.ts
+++ functions/setCompletion.ts
@@ -1,7 +1,15 @@
 import { SetCompletion, Todo } from "@teamkeel/sdk";
 
-export default SetCompletion(async (inputs, api) => {
+export default SetCompletion(async (inputs, api, ctx) => {
   const now = new Date();
+
+  const todo = await api.models.todo.findOne(inputs.where);
+
+  if (todo && todo.ownerId == ctx.identity?.id) {
+    api.permissions.allow();
+  } else {
+    api.permissions.deny();
+  }
 
   const values: Partial<Todo> = {
     ...inputs.values,
